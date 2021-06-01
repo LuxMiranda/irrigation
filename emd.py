b97cb92e1e429e0ff44f15ac1ad26299852c18ec
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2021-06-01 18:14:50 -0400

    Fully working EMD pipelinegit add emd.py environment.yml !

--- emd.py
+++ emd.py
@@ -6,14 +6,14 @@ netlogoPath = '/home/lux/netlogo-6.0.2/'
 # Model path. Note that the "./" is necessary. 
 modelPath = './main.nlogo'
 # Setup commands
-setup = ['setup', 'calibrate']
+setup = ['setup']
 # Measurement reporters
 measurements = ['fit']
 # Number of ticks to run each simulation for
-ticks = 100
+ticks = 14
 
 # Initialize EMD
-emd = EvolutionaryModelDiscovery(netlogoPath, modelPath, setup, measurements, ticks)
+emd = EvolutionaryModelDiscovery(netlogoPath, modelPath, setup, measurements, ticks, go_command='calibrate')
 
 # The fitness function, defined simply as "fit" in the literature
 def fitness(results):
@@ -24,7 +24,8 @@ def fitness(results):
 emd.set_replications(1)
 emd.set_mutation_rate(0.1)
 emd.set_crossover_rate(0.8)
-emd.set_generations(20)
+emd.set_generations(1)
+#emd.set_generations(20)
 emd.set_is_minimize(True)
 
 
@@ -33,4 +34,6 @@ emd.set_objective_function(fitness)
 
 if __name__ == '__main__':
     print(emd.evolve())
+    print('Here the thing!')
+
     emd.shutdown()
